aca89dc56a044d17192df50e5137566e468f8dec
akhil2502 <akhilachilies>
2021-08-10 18:30:11 +0530

    [akhil] Add:uc1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HashTableProblem
+{
+    class Program
+    {
+        static readonly MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        public static void HashOperation()
+        {
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash0 = hash.Get("0");
+            Console.WriteLine("Oth index value:" + hash0);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value:" + hash3);
+            hash.Remove(hash3);
+        }
+        static void Main(string[] args)
+        {
+            Program.HashOperation();
+        }
+    }
+}
